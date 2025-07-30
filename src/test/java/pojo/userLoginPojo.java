0445906f9e1c497690ff1cef2a271e4e0d28c7fa
SumathiMukkera <sumathi.barupati9@gmail.com>
2025-07-30 16:45:21 -0500

    user login admin token generation

--- src/test/java/pojo/userLoginPojo.java
+++ src/test/java/pojo/userLoginPojo.java
@@ -2,4 +2,48 @@ package pojo;
 
 public class userLoginPojo {
 
+    private String userLoginEmail; // match JSON key
+    private String password;       // lowercase to match JSON key
+    private String adminToken;
+    
+       private String endpoint;
+
+    public userLoginPojo() {}
+
+    public userLoginPojo(String userLoginEmail, String password) {
+        this.userLoginEmail = userLoginEmail;
+        this.password = password;
+    }
+
+    public String getUserLoginEmail() {
+        return userLoginEmail;
+    }
+
+    public void setUserLoginEmail(String userLoginEmail) {
+        this.userLoginEmail = userLoginEmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAdminToken() {
+        return adminToken;
+    }
+
+    public void setAdminToken(String adminToken) {
+        this.adminToken = adminToken;
+    }
+
+    public String getEndpoint() {
+        return endpoint;
+    }
+
+    public void setEndpoint(String endpoint) {
+        this.endpoint = endpoint;
+    }
 }
--- src/test/java/runner/testRunner.java
+++ src/test/java/runner/testRunner.java
@@ -1,5 +1,20 @@
 package runner;
 
-public class testRunner {
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+		
+		
+		features = {"src\\test\\resources\\features"},
+	    glue = "stepDefinitions",
+	    plugin = {"pretty", "html:target/cucumber-report.html",
+	    		"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},// Allure plugin for cucumber v7
+	     //tags = " @test4", 
+	    monochrome = true
+		)
+
+
+public class testRunner extends AbstractTestNGCucumberTests {
 
 }
