0668bae7ecd0dc4c7bdbb2e8f8dd8c778428813a
bhoomi <bhoomi.chakrani@hotmail.com>
2025-08-02 14:46:00 -0400

    Negative scenarios create patient

--- src/test/java/stepDefinitions/createPatientStepsDef.java
+++ src/test/java/stepDefinitions/createPatientStepsDef.java
@@ -230,4 +230,305 @@ public class createPatientStepsDef {
 		response.then().log().all().statusCode(expectedStatusCode);
 		assertTrue(response.jsonPath().getString("errorMessage").contains("Access is denied"));
 	}
+	
+	@Given("Dietician creates POST request only by valid mandatory details into the form-data key and value fields with {string}.")
+	public void dietician_creates_post_request_only_by_valid_mandatory_details_into_the_form_data_key_and_value_fields_with(String scenarioName) throws IOException {
+	    ObjectMapper mapper = new ObjectMapper();
+	    
+	    patientDataList = mapper.readValue(
+		        new File("src/test/resources/testData/patientData.json"),
+		        new TypeReference<List<PatientTestData>>() {}
+		    );
+	    
+	    for(PatientTestData patientData : patientDataList) {
+	    	if(patientData.getScenarioName().equals(scenarioName)) {
+	    		PatientPojo patientPojo = patientData.getPatientDataInput();
+	    		String patientInfoJson = mapper.writeValueAsString(patientPojo);
+	    		System.out.println("Sending patientInfo as: " + patientInfoJson);
+	    		
+	    	    request = given()
+	    	    	.baseUri(configReader.getProperty("baseURL"))
+	    	    	.header("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ2YWhhMTBAZ21haWwuY29tIiwiaWF0IjoxNzU0MDk3MTg5LCJleHAiOjE3NTQxMjU5ODl9.nV82gp6J5zzpZjeznLd_WOcxSRPANki9TB8ZaaWd9A9ngxSnxlduqoTHj5d2EL6sUh992gf11Ux64mR23GuYTA")
+	    	    	.contentType(ContentType.MULTIPART)
+	    	   		.multiPart("patientInfo", patientInfoJson);
+	    	    
+	    	    System.out.println("POST request sent: Breaking loop");
+	    	    break;
+	    	}
+	    }
+	}
+	
+	@Given("Dietician creates POST request only by valid additional details into the form-data key and value fields with {string}.")
+	public void dietician_creates_post_request_only_by_valid_additional_details_into_the_form_data_key_and_value_fields_with(String scenarioName) throws IOException {
+	    ObjectMapper mapper = new ObjectMapper();
+	    
+	    patientDataList = mapper.readValue(
+		        new File("src/test/resources/testData/patientData.json"),
+		        new TypeReference<List<PatientTestData>>() {}
+		    );
+	    
+	    for(PatientTestData patientData : patientDataList) {
+	    	if(patientData.getScenarioName().equals(scenarioName)) {
+	    		PatientPojo patientPojo = patientData.getPatientDataInput();
+	    		String patientInfoJson = mapper.writeValueAsString(patientPojo);
+	    		System.out.println("Not Sending patientInfo as: " + patientInfoJson);
+	    		
+	    		VitalsPojo vitalsPojo = patientData.getVitalsDataInput();
+	    		String vitalsInfoJson = mapper.writeValueAsString(vitalsPojo);
+	    		System.out.println("Sending vitalsInfo as: " + vitalsInfoJson);
+	    		
+	    		File file = new File(configReader.getProperty("pdfPath"));
+	    		String filePath = file.getAbsolutePath();
+	    		System.out.println("Sending file as: " +filePath);
+	    		
+	    	    request = given()
+	    	    	.baseUri(configReader.getProperty("baseURL"))
+	    	    	.header("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ2YWhhMTBAZ21haWwuY29tIiwiaWF0IjoxNzU0MTUwODM0LCJleHAiOjE3NTQxNzk2MzR9.jYjhBgld1fyf0dbagCmAD33Qloakv9H3qZO7ILnJlnoOyc2cHcAkbgvWTO5eDS48Y9Dk15DvKHmwCiFe-Kg2sQ")
+	    	    	.contentType(ContentType.MULTIPART)
+	    		    .multiPart("vitals", vitalsInfoJson);
+	    		    //.multiPart("file", new File(configReader.getProperty("pdfPath")));
+	    	    
+	    	    System.out.println("POST request sent: Breaking loop");
+	    	    break;
+	    	}
+	    }
+	}
+
+	@Then("Dietician recieves {int} Bad request after create patient request")
+	public void dietician_recieves_bad_request_after_create_patient_request(Integer expectedStatusCode) {
+		response.then().log().all().statusCode(expectedStatusCode);
+		assertTrue(response.jsonPath().getString("error").contains("Bad Request"));
+		assertTrue(response.jsonPath().getString("message").contains("unexpected error"));
+	}
+	
+	@Given("Dietician creates POST request only by invalid mandatory details into the form-data key and value fields with {string}.")
+	public void dietician_creates_post_request_only_by_invalid_mandatory_details_into_the_form_data_key_and_value_fields_with(String scenarioName) throws IOException {
+	    ObjectMapper mapper = new ObjectMapper();
+	    
+	    patientDataList = mapper.readValue(
+		        new File("src/test/resources/testData/patientData.json"),
+		        new TypeReference<List<PatientTestData>>() {}
+		    );
+	    
+	    for(PatientTestData patientData : patientDataList) {
+	    	if(patientData.getScenarioName().equals(scenarioName)) {
+	    		PatientPojo patientPojo = patientData.getPatientDataInput();
+	    		String patientInfoJson = mapper.writeValueAsString(patientPojo);
+	    		System.out.println("Sending patientInfo as: " + patientInfoJson);
+	    		
+	    		VitalsPojo vitalsPojo = patientData.getVitalsDataInput();
+	    		String vitalsInfoJson = mapper.writeValueAsString(vitalsPojo);
+	    		System.out.println("Sending vitalsInfo as: " + vitalsInfoJson);
+	    		
+	    		File file = new File(configReader.getProperty("pdfPath"));
+	    		String filePath = file.getAbsolutePath();
+	    		System.out.println("Sending file as: " +filePath);
+	    		
+	    	    request = given()
+	    	    	.baseUri(configReader.getProperty("baseURL"))	    	    	
+	    	    	.contentType(ContentType.MULTIPART)
+	    	    	.header("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ2YWhhMTBAZ21haWwuY29tIiwiaWF0IjoxNzU0MTUwODM0LCJleHAiOjE3NTQxNzk2MzR9.jYjhBgld1fyf0dbagCmAD33Qloakv9H3qZO7ILnJlnoOyc2cHcAkbgvWTO5eDS48Y9Dk15DvKHmwCiFe-Kg2sQ")
+	    	   		.multiPart("patientInfo", patientInfoJson)
+	    		    .multiPart("vitals", vitalsInfoJson);
+	    		    //.multiPart("file", new File(configReader.getProperty("pdfPath")));
+	    	    
+	    	    System.out.println("POST request sent: Breaking loop");
+	    	    break;
+	    	}
+	    }	    
+	}
+	
+	@Then("Dietician recieves {int} Bad request after create patient request with {string}")
+	public void dietician_recieves_bad_request_after_create_patient_request_with(Integer expectedStatusCode, String errorMessage) {
+		response.then().log().all().statusCode(expectedStatusCode);
+		assertTrue(response.jsonPath().getString("errorCode").contains("INVALID_REQ_DATA"));
+		assertTrue(response.jsonPath().getString("errorMessage").contains(errorMessage));
+	}
+	
+//	@Given("Dietician creates POST request only by invalid additional details into the form-data key and value fields with {string}.")
+//	public void dietician_creates_post_request_only_by_invalid_additional_details_into_the_form_data_key_and_value_fields_with(String scenarioName) throws IOException {
+//	    ObjectMapper mapper = new ObjectMapper();
+//	    
+//	    patientDataList = mapper.readValue(
+//		        new File("src/test/resources/testData/patientData.json"),
+//		        new TypeReference<List<PatientTestData>>() {}
+//		    );
+//	    
+//	    for(PatientTestData patientData : patientDataList) {
+//	    	if(patientData.getScenarioName().equals(scenarioName)) {
+//	    		PatientPojo patientPojo = patientData.getPatientDataInput();
+//	    		String patientInfoJson = mapper.writeValueAsString(patientPojo);
+//	    		System.out.println("Sending patientInfo as: " + patientInfoJson);
+//	    		
+//	    		VitalsPojo vitalsPojo = patientData.getVitalsDataInput();
+//	    		String vitalsInfoJson = mapper.writeValueAsString(vitalsPojo);
+//	    		System.out.println("Sending vitalsInfo as: " + vitalsInfoJson);
+//	    		
+//	    		File file = new File(configReader.getProperty("pdfPath"));
+//	    		String filePath = file.getAbsolutePath();
+//	    		System.out.println("Sending file as: " +filePath);
+//	    		
+//	    	    request = given()
+//	    	    	.baseUri(configReader.getProperty("baseURL"))	    	    	
+//	    	    	.contentType(ContentType.MULTIPART)
+//	    	    	.header("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ2YWhhMTBAZ21haWwuY29tIiwiaWF0IjoxNzU0MTUwODM0LCJleHAiOjE3NTQxNzk2MzR9.jYjhBgld1fyf0dbagCmAD33Qloakv9H3qZO7ILnJlnoOyc2cHcAkbgvWTO5eDS48Y9Dk15DvKHmwCiFe-Kg2sQ")
+//	    	   		.multiPart("patientInfo", patientInfoJson)
+//	    		    .multiPart("vitals", vitalsInfoJson);
+//	    		    //.multiPart("file", new File(configReader.getProperty("pdfPath")));
+//	    	    
+//	    	    System.out.println("POST request sent: Breaking loop");
+//	    	    break;
+//	    	}
+//	    }	    
+//	}
+	
+	@Given("Dietician creates PUT request by entering valid data into the form-data key and value fields with {string}.")
+	public void dietician_creates_put_request_by_entering_valid_data_into_the_form_data_key_and_value_fields_with(String scenarioName) throws IOException {
+	    ObjectMapper mapper = new ObjectMapper();
+	    
+	    patientDataList = mapper.readValue(
+		        new File("src/test/resources/testData/patientData.json"),
+		        new TypeReference<List<PatientTestData>>() {}
+		    );
+	    
+	    for(PatientTestData patientData : patientDataList) {
+	    	if(patientData.getScenarioName().equals(scenarioName)) {
+	    		PatientPojo patientPojo = patientData.getPatientDataInput();
+	    		String patientInfoJson = mapper.writeValueAsString(patientPojo);
+	    		System.out.println("Sending patientInfo as: " + patientInfoJson);
+	    		
+	    		VitalsPojo vitalsPojo = patientData.getVitalsDataInput();
+	    		String vitalsInfoJson = mapper.writeValueAsString(vitalsPojo);
+	    		System.out.println("Sending vitalsInfo as: " + vitalsInfoJson);
+	    		
+	    		File file = new File(configReader.getProperty("pdfPath"));
+	    		String filePath = file.getAbsolutePath();
+	    		System.out.println("Sending file as: " +filePath);
+	    		
+	    	    request = given()
+	    	    	.baseUri(configReader.getProperty("baseURL"))	    	    	
+	    	    	.contentType(ContentType.MULTIPART)
+	    	    	.header("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ2YWhhMTBAZ21haWwuY29tIiwiaWF0IjoxNzU0MTUwODM0LCJleHAiOjE3NTQxNzk2MzR9.jYjhBgld1fyf0dbagCmAD33Qloakv9H3qZO7ILnJlnoOyc2cHcAkbgvWTO5eDS48Y9Dk15DvKHmwCiFe-Kg2sQ")
+	    	   		.multiPart("patientInfo", patientInfoJson)
+	    		    .multiPart("vitals", vitalsInfoJson);
+	    		    //.multiPart("file", new File(configReader.getProperty("pdfPath")));
+	    	    
+	    	    System.out.println("POST request sent: Breaking loop");
+	    	    break;
+	    	}
+	    }	    
+	}
+
+	@When("Dietician send PUT http request with endpoint to create patient with {string}")
+	public void dietician_send_put_http_request_with_endpoint_to_create_patient_with(String scenarioName) {
+	    for(PatientTestData patientData : patientDataList) {
+	    	if(patientData.getScenarioName().equals(scenarioName)) {
+	    		response = request.when().put(patientData.getEndpoint());
+	    	}
+	    }
+	}
+
+	@Then("Dietician recieves {int} method not allowed after create patient request with {string}")
+	public void dietician_recieves_method_not_allowed_after_create_patient_request_with(Integer expectedStatusCode, String errorMessage) {
+		response.then().log().all().statusCode(expectedStatusCode);
+		assertTrue(response.jsonPath().getString("error").contains("Method Not Allowed"));
+		assertTrue(response.jsonPath().getString("message").contains(errorMessage));
+	}
+	
+	@Given("Dietician creates POST request with valid token by entering valid data into the form-data key and value fields with {string}.")
+	public void dietician_creates_post_request_with_valid_token_by_entering_valid_data_into_the_form_data_key_and_value_fields_with(String scenarioName) throws IOException {
+	    ObjectMapper mapper = new ObjectMapper();
+	    
+	    patientDataList = mapper.readValue(
+		        new File("src/test/resources/testData/patientData.json"),
+		        new TypeReference<List<PatientTestData>>() {}
+		    );
+	    
+	    for(PatientTestData patientData : patientDataList) {
+	    	if(patientData.getScenarioName().equals(scenarioName)) {
+	    		PatientPojo patientPojo = patientData.getPatientDataInput();
+	    		String patientInfoJson = mapper.writeValueAsString(patientPojo);
+	    		System.out.println("Sending patientInfo as: " + patientInfoJson);
+	    		
+	    		VitalsPojo vitalsPojo = patientData.getVitalsDataInput();
+	    		String vitalsInfoJson = mapper.writeValueAsString(vitalsPojo);
+	    		System.out.println("Sending vitalsInfo as: " + vitalsInfoJson);
+	    		
+	    		File file = new File(configReader.getProperty("pdfPath"));
+	    		String filePath = file.getAbsolutePath();
+	    		System.out.println("Sending file as: " +filePath);
+	    		
+	    	    request = given()
+	    	    	.baseUri(configReader.getProperty("baseURL"))	    	    	
+	    	    	.contentType(ContentType.MULTIPART)
+	    	    	.header("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ2YWhhMTBAZ21haWwuY29tIiwiaWF0IjoxNzU0MTUwODM0LCJleHAiOjE3NTQxNzk2MzR9.jYjhBgld1fyf0dbagCmAD33Qloakv9H3qZO7ILnJlnoOyc2cHcAkbgvWTO5eDS48Y9Dk15DvKHmwCiFe-Kg2sQ")
+	    	   		.multiPart("patientInfo", patientInfoJson)
+	    		    .multiPart("vitals", vitalsInfoJson);
+	    		    //.multiPart("file", new File(configReader.getProperty("pdfPath")));
+	    	    
+	    	    System.out.println("POST request sent: Breaking loop");
+	    	    break;
+	    	}
+	    }	    
+	}
+
+	@When("Dietician send POST http request with invalid endpoint to create patient with {string}")
+	public void dietician_send_post_http_request_with_invalid_endpoint_to_create_patient_with(String scenarioName) {
+	    for(PatientTestData patientData : patientDataList) {
+	    	if(patientData.getScenarioName().equals(scenarioName)) {
+	    		response = request.when().post(patientData.getEndpoint());
+	    	}
+	    }
+	}
+
+	@Then("Dietician recieves {int} not found after create patient request with {string}")
+	public void dietician_recieves_not_found_after_create_patient_request_with(Integer expectedStatusCode, String errorMessage) {
+		response.then().log().all().statusCode(expectedStatusCode);
+		assertTrue(response.jsonPath().getString("error").contains("Not Found"));
+		assertTrue(response.jsonPath().getString("message").contains(errorMessage));
+	}
+	
+	@Given("Dietician creates POST request by entering valid data into the form-data key and value fields and invalid content type with {string}")
+	public void dietician_creates_post_request_by_entering_valid_data_into_the_form_data_key_and_value_fields_and_invalid_content_type_with(String scenarioName) throws IOException {
+	    ObjectMapper mapper = new ObjectMapper();
+	    
+	    patientDataList = mapper.readValue(
+		        new File("src/test/resources/testData/patientData.json"),
+		        new TypeReference<List<PatientTestData>>() {}
+		    );
+	    
+	    for(PatientTestData patientData : patientDataList) {
+	    	if(patientData.getScenarioName().equals(scenarioName)) {
+	    		PatientPojo patientPojo = patientData.getPatientDataInput();
+	    		String patientInfoJson = mapper.writeValueAsString(patientPojo);
+	    		System.out.println("Sending patientInfo as: " + patientInfoJson);
+	    		
+	    		VitalsPojo vitalsPojo = patientData.getVitalsDataInput();
+	    		String vitalsInfoJson = mapper.writeValueAsString(vitalsPojo);
+	    		System.out.println("Sending vitalsInfo as: " + vitalsInfoJson);
+	    		
+	    		File file = new File(configReader.getProperty("pdfPath"));
+	    		String filePath = file.getAbsolutePath();
+	    		System.out.println("Sending file as: " +filePath);
+	    		
+	    	    request = given()
+	    	    	.baseUri(configReader.getProperty("baseURL"))	    	    	
+	    	    	.contentType(ContentType.URLENC)
+	    	    	.header("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ2YWhhMTBAZ21haWwuY29tIiwiaWF0IjoxNzU0MTUwODM0LCJleHAiOjE3NTQxNzk2MzR9.jYjhBgld1fyf0dbagCmAD33Qloakv9H3qZO7ILnJlnoOyc2cHcAkbgvWTO5eDS48Y9Dk15DvKHmwCiFe-Kg2sQ")
+	    	   		.formParam("patientInfo", patientInfoJson)
+	    		    .formParam("vitals", vitalsInfoJson);
+	    		    //.multiPart("file", new File(configReader.getProperty("pdfPath")));
+	    	    
+	    	    System.out.println("POST request sent: Breaking loop");
+	    	    break;
+	    	}
+	    }	    
+	}
+
+	@Then("Dietician recieves {int} unsupported media type after create patient request with {string}")
+	public void dietician_recieves_unsupported_media_type_after_create_patient_request_with(Integer expectedStatusCode, String errorMessage) {
+		response.then().log().all().statusCode(expectedStatusCode);
+		assertTrue(response.jsonPath().getString("error").contains("Unsupported Media Type"));
+		assertTrue(response.jsonPath().getString("message").contains(errorMessage));
+	}
 }
--- src/test/resources/features/patientController/CreatePatientDieticianToken.feature
+++ src/test/resources/features/patientController/CreatePatientDieticianToken.feature
@@ -11,38 +11,68 @@ Then Dietician recieves 201 created and with response body. (Auto created dietic
 		| scenarioName    |
 		| Create patient  |
 
-#Scenario: Check dietician able to create patient only with valid mandatory details
-#Given Dietician creates POST request only by valid mandatory details into the form-data key and value fields.
-#When Dietician send POST http request with endpoint to create patient
-#Then Dietician recieves 201 created and with response body. (Auto created dietician ID and login password)
-#
-#Scenario: Check dietician able to create patient only with valid additional details
-#Given Dietician creates POST request only by valid additional details into the form-data key and value fields.
-#When Dietician send POST http request with endpoint to create patient
+Scenario Outline: Check dietician able to create patient only with valid mandatory details
+Given Dietician creates POST request only by valid mandatory details into the form-data key and value fields with "<scenarioName>".
+When Dietician send POST http request with endpoint to create patient with "<scenarioName>"
+Then Dietician recieves 201 created and with response body. (Auto created dietician ID and login password)
+	Examples:
+		| scenarioName    |
+		| Create patient with mandatory fields only  |
+		
+Scenario Outline: Check dietician able to create patient only with valid additional details
+Given Dietician creates POST request only by valid additional details into the form-data key and value fields with "<scenarioName>".
+When Dietician send POST http request with endpoint to create patient with "<scenarioName>"
+Then Dietician recieves 400 Bad request after create patient request
+	Examples:
+		| scenarioName    |
+		| Create patient with valid additional details only |
+		
+Scenario Outline: Check dietician able to create patient with invalid data (mandatory details)
+Given Dietician creates POST request only by invalid mandatory details into the form-data key and value fields with "<scenarioName>".
+When Dietician send POST http request with endpoint to create patient with "<scenarioName>"
+Then Dietician recieves 400 Bad request after create patient request with "<errorMessage>"
+	Examples:
+		|           scenarioName                     |            errorMessage                               |
+		| Create patient with invalid firstname      |          FirstName should only contain Alphabets      |
+		| Create patient with invalid lastname       |          LastName should only contain Alphabets       |
+		| Create patient with invalid contactnumber  | Contact number should contain 10 digits               |
+		| Create patient with contactnumber more than 10 digits | Contact number should contain 10 digits    |
+		| Create patient with contactnumber less than 10 digits | Contact number should contain 10 digits    |
+		| Create patient with invalid email          |  Email should start with a letter                     |
+		| Create patient with invalid allergy        |  walnut, pistachio, sesame, hazelnut, pecan, cashew   |
+		| Create patient with invalid FoodPreference | VEGAN, VEGETARIAN, JAIN, EGGETARIAN, NONVEG           |
+		| Create patient with invalid CuisineCategory| Invalid Cuisine Category                              |
+#		| Create patient with invalid DateOfBirth    | |
+		 
+#Scenario Outline: Check dietician able to create patient with valid mandatory fields and invalid data (additional details)
+#Given Dietician creates POST request only by invalid additional details into the form-data key and value fields with "<scenarioName>".
+#When Dietician send POST http request with endpoint to create patient with "<scenarioName>"
 #Then Dietician recieves 400 Bad request after create patient request
-#
-#Scenario: Check dietician able to create patient with invalid data (mandatory details)
-#Given Dietician creates POST request only by invalid mandatory details into the form-data key and value fields.
-#When Dietician send POST http request with endpoint to create patient
-#Then Dietician recieves 400 Bad request after create patient request
-#
-#Scenario: Check dietician able to create patient with valid mandatory fields and invalid data (additional details)
-#Given Dietician creates POST request only by invalid additional details into the form-data key and value fields.
-#When Dietician send POST http request with endpoint to create patient
-#Then Dietician recieves 400 Bad request after create patient request
-#
-#Scenario: Check dietician able to create patient with valid data and invalid method
-#Given Dietician creates PUT request by entering valid data into the form-data key and value fields.
-#When Dietician send PUT http request with endpoint to create patient
-#Then Dietician recieves 405 method not allowed after create patient request
-#
-#Scenario: Check dietician able to create patient with valid data and invalid endpoint
-#Given Dietician creates POST request by entering valid data into the form-data key and value fields.
-#When Dietician send POST http request with invalid endpoint to create patient
-#Then Dietician recieves 404 not found after create patient request
-#
-#Scenario: Check dietician able to create patient with valid data and invalid content type
-#Given Dietician creates POST request by entering valid data into the form-data key and value fields and invalid content type
-#When Dietician send POST http request with endpoint to create patient
-#Then Dietician recieves 415 unsupported media type after create patient request
+#	Examples:
+#		|           scenarioName                     |            errorMessage                               |
+#		| Create patient with invalid weight in vitals | |
+#		| Create patient with invalid height in vitals | |
+#		
+Scenario Outline: Check dietician able to create patient with valid data and invalid method
+Given Dietician creates PUT request by entering valid data into the form-data key and value fields with "<scenarioName>".
+When Dietician send PUT http request with endpoint to create patient with "<scenarioName>"
+Then Dietician recieves 405 method not allowed after create patient request with "<errorMessage>"
+	Examples:
+		|           scenarioName                     |            errorMessage                               |
+		| Create patient with invalid method PUT     | HTTP method is not supported for this endpoint        |
 
+Scenario Outline: Check dietician able to create patient with valid data and invalid endpoint
+Given Dietician creates POST request with valid token by entering valid data into the form-data key and value fields with "<scenarioName>".
+When Dietician send POST http request with invalid endpoint to create patient with "<scenarioName>"
+Then Dietician recieves 404 not found after create patient request with "<errorMessage>"
+	Examples:
+		|           scenarioName                     |            errorMessage                  |
+		| Create patient with invalid endPoint       |      Endpoint not supported.             |
+		
+Scenario Outline: Check dietician able to create patient with valid data and invalid content type
+Given Dietician creates POST request by entering valid data into the form-data key and value fields and invalid content type with "<scenarioName>"
+When Dietician send POST http request with endpoint to create patient with "<scenarioName>"
+Then Dietician recieves 415 unsupported media type after create patient request with "<errorMessage>"
+	Examples:
+		|           scenarioName                     |            errorMessage         |
+    | Create patient with invalid contentType    |    unexpected error             |
