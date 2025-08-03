b9f1e497612b29c72d9e5bd5d17b911dd8bc0515
Sumathi Mukkera <sumathi.barupati9@gmail.com>
2025-08-03 00:35:00 -0500

    Update configReader.java

--- src/test/java/utilities/configReader.java
+++ src/test/java/utilities/configReader.java
@@ -1,39 +1,9 @@
 package utilities;
 
 import java.io.FileInputStream;
-<<<<<<< HEAD
+
 import java.io.IOException;
 import java.util.Properties;
-=======
-import java.util.Properties;
-
-public class configReader {
-	
-	static Properties prop = new Properties();
-	public static Properties getProperties() {
-		
-		try {
-		
-		String projectpath = System.getProperty("user.dir");
-		FileInputStream file = new FileInputStream(projectpath+ "\\src\\test\\resources\\config.properties");
-		prop.load(file);
-		}
-		catch(Exception ex) {
-			
-			ex.printStackTrace();
-		}
-		
-		return prop;
-	}
-	
-	public static String getProperty(String key) {
-		getProperties();
-        if (prop == null) {
-            throw new RuntimeException("Properties file not initialized!");
-        }
-        return prop.getProperty(key);
-    }
->>>>>>> origin/Sumathi
 
 public class configReader {
     
